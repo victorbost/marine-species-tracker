252b4e021ec20b30448165b9e7a91952b9746064
victordocent <victor.bostaetter@docent-art.com>
2025-10-20 18:07:29 +0200

    Add user authentication and registration functionality
    - Updated requirements to include djangorestframework-simplejwt for JWT authentication.
    - Changed database host configuration in settings.
    - Implemented user registration and detail views with serializers.
    - Added URL patterns for user authentication endpoints.
    - Created empty URL configurations for observations and species modules.

---
+++ backend/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from . import views
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('user/', views.UserDetailView.as_view(), name='auth_user'),
+]
---
+++ backend/users/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
